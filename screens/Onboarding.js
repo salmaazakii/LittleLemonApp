84dafa0d01ee763a92e16d8f2c70cdc46db2e479
salmaazakii <salmazaki700@gmail.com>
2023-09-28 16:02:56 +0300

    design Onboarding screen

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,4 +1,5 @@
-import {View,Image,TextInput,Button} from 'react-native'
+import {View,Image,TextInput,Pressable,Text} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
 
 function Header(){
     return(
@@ -9,8 +10,18 @@ function Header(){
 }
 export default function Onboarding(){
     return(
-        <View style={{flex:1,backgroundColor:'red'}}>
+        <LinearGradient colors={['#f5e7ae','#faf8f2']} style={{flex:1, alignItems:'center'}}>
             <Header />
-        </View>
+            <Text style={{marginTop:20,fontSize:20,fontWeight:'500', color:'#495E57',fontFamily:'Karla-Regular'}}>Let us get to know you</Text>
+            <View style={{marginVertical:20,alignItems:'flex-start',width:'70%',height:'20%',justifyContent:'space-between'}}>
+                <Text style={{fontFamily:'Karla-Regular',fontSize:16}}>First Name</Text>
+                <TextInput style={{borderRadius:10,borderWidth:1,width:'100%'}}></TextInput>
+                <Text style={{fontFamily:'Karla-Regular',fontSize:16,marginTop:20}}>Email</Text>
+                <TextInput style={{borderRadius:10,borderWidth:1,width:'100%'}}></TextInput>
+            </View>
+            <Pressable style={{marginTop:20,width:'25%',height:50,backgroundColor:'#495E57',borderRadius:10,alignItems:'center',justifyContent:'center'}}>
+                <Text style={{color:'white',fontSize:18}}>Next</Text>
+            </Pressable>
+        </LinearGradient>
     );
 }
